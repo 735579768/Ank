37cf1e040cbb6ace801313895838de7f7063d6a9
735579768@qq.com <735579768@qq.com>
2016-06-30 14:51:40 +0800

    添加公共配置文件

---
+++ app/config/common/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	//数据库配置
+	'db_config' => [
+		'database_type' => 'mysql',
+		'database_name' => 'name',
+		'server'        => 'localhost',
+		'username'      => 'your_username',
+		'password'      => 'your_password',
+		'charset'       => 'utf8',
+	],
+];
