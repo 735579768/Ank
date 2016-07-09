6d12a99601caf9a2aa868b045fb4344ed18fb676
kelicom <735579768@qq.com>
2016-07-09 18:19:33 +0800

    添加缓存类

--- vendor/ainiku/cache.php
+++ vendor/ainiku/cache.php
@@ -6,39 +6,73 @@ class Cache {
 	static public $_instance = null;
 
 	public function getInstance() {
-		if (!\ainiku\Log::$_instance) {
-			$log       = new Logger('Ainiku');
-			$file_path = DATA_PATH . '/cache/' . BIND_MODULE . '/data/' . date('y-m-d') . '.log';
+		if (!\ainiku\Cache::$_instance) {
+			//$log        = new Logger('Ainiku');
+			$cache_path = DATA_PATH . '/cache/' . BIND_MODULE . '/data/';
 			if (!file_exists($file_path)) {
 				mkdir(dirname($file_path), 0777, true);
-				file_put_contents(file_path, '');
+				// file_put_contents(file_path, '');
 			}
-			$stream = new StreamHandler($file_path, Logger::DEBUG);
-			$log->pushHandler($stream);
-			\ainiku\Log::$_instance = $log;
+			\ainiku\Cache::$_instance = new \Doctrine\Common\Cache\FilesystemCache($cache_path);
 
 		}
-		return \ainiku\Log::$_instance;
+		return \ainiku\Cache::$_instance;
 	}
 	/**
 	 * 写缓存
 	 * @param  string  $key      缓存key
 	 * @param  [type]  $data     缓存的数据
-	 * @param  integer $lifeTime 保存的时间
+	 * @param  integer $lifeTime 保存的时间false为永久
 	 * @return [type]            boolean
 	 */
-	static public function write($key = '', $data = null, $lifeTime = 60) {
-
+	static public function write($key = '', $data = null, $lifeTime = false) {
+		return \ainiku\Cache::getInstance()->save($key, $data, $lifeTime);
 	}
+	/**
+	 * 写临时缓存
+	 * @param  string $key 缓存key
+	 * @return [type]      [description]
+	 */
+	static public function writeTemp($key = '', $data = null, $lifeTime = false) {
+		if ($lifeTime === false) {
+			$lifeTime = \ainiku\app::getConfig('cache_time');
+		}
+		return \ainiku\Cache::getInstance()->save($key, $data, $lifeTime);
+	}
+
 	/**
 	 * 读缓存
 	 * @param  string $key 缓存key
 	 * @return [type]      [description]
 	 */
 	static public function read($key = '') {
-
+		return \ainiku\Cache::getInstance()->fetch($key);
+	}
+	/**
+	 * 删除缓存
+	 * @param  string $key 缓存key
+	 * @return [type]      [description]
+	 */
+	static public function delete($key = '') {
+		return \ainiku\Cache::getInstance()->delete($key);
 	}
 
+	/**
+	 * 检查缓存是否存在
+	 * @param  string $key [description]
+	 * @return [type]      [description]
+	 */
+	static public function exists($key = '') {
+		return \ainiku\Cache::getInstance()->contains($key);
+	}
+	/**
+	 * 删除全部缓存
+	 * @param  string $key 缓存key
+	 * @return [type]      [description]
+	 */
+	static public function deleteAll($key = '') {
+		return \ainiku\Cache::getInstance()->deleteAll($key);
+	}
 	/**
 	 * 析构方法
 	 * @access public
--- vendor/ainiku/config.php
+++ vendor/ainiku/config.php
@@ -7,4 +7,7 @@ return [
 	'default_action'     => 'index',
 	//默认系统url配置
 	'url_model'          => '0',
+
+	//缓存配置
+	'cache_time'         => 60, //单位秒
 ];
