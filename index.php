1501dd9bb8ba4f6fa1badf3e7bc90c31325d6274
kelicom <735579768@qq.com>
2016-07-10 18:25:22 +0800

    add  frame init file

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
+//开启调试模式
 define('APP_DEBUG', true);
+//入口文件目录绝对路径
 define('__SITE_ROOT__', str_replace('\\', '/', __DIR__));
+//app目录绝对目录
 define('APP_PATH', __SITE_ROOT__ . '/app');
+//数据缓存目录绝对目录
 define('DATA_PATH', __SITE_ROOT__ . '/data');
+//包含自动加载类
 $loader = require __SITE_ROOT__ . '/vendor/autoload.php';
-define('BIND_MODULE', 'home');
-\ainiku\app::start();
-// require './vendor/ainiku/ank.php';
+//加载框架初始化文件
+require './vendor/ainiku/ank.php';
---
+++ vendor/ainiku/ank.php
@@ -0,0 +1,3 @@
+<?php
+defined('BIND_MODULE') or define('BIND_MODULE', 'home');
+\ainiku\app::start();
